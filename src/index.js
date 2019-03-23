0012f8b6acf0a8d4c45f327e88eeaf3b1123cf4a
Saadh <atextbooksituation@gmail.com>
2019-03-22 22:06:04 -0400

    Made a mistake for the location of Surge.sh’s CNAME file config. Also testing webhook integrations (not related to this project specifically) and cleaning out CRA readme content

--- src/index.js
+++ src/index.js
@@ -19,3 +19,4 @@ ReactDOM.render( jsx, document.getElementById( 'root' ) );
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
