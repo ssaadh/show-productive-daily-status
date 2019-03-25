0535ec468a50e4d37c4898da70d9251056acb2dc
Saadh S <atextbooksituation@gmail.com>
2019-03-25 15:18:30 -0400

    Squashed a few commits into this one. Reducer is now a HOC reducer. Have to specify an identifier for which service the reducer will be for. Then some general JS code in an incorrectly named configureStore in oauth/redux. To make the creation of multiple reducers and initial state easier.

---
+++ src/libs/oauth/redux/configureStore.js
@@ -0,0 +1,23 @@
+import localAuth from '../localAuth';
+import oauth2Reducer from './reducer';
+
+const oauthStateTemplate = ( identifier ) => ( { 
+  loggingIn: false,
+  error: null, 
+
+  loggedIn: localAuth.validToken( identifier ) ? true : false, 
+  token: localAuth.validToken( identifier ), 
+  type: localAuth.get( identifier, 'token_type' ) 
+} );
+
+const oauthKeyTemplate = ( identifier ) => `oauth/${ identifier.toLowerCase() }`;
+
+const combinedOauthStateHash = ( identifier ) => ( {
+  [ oauthKeyTemplate( identifier ) ]: oauthStateTemplate( identifier )
+} );
+
+const combinedOauthReducerHash = ( identifier ) => ( {
+  [ oauthKeyTemplate( identifier ) ]: oauth2Reducer( identifier )
+} );
+
+export { combinedOauthStateHash, combinedOauthReducerHash };
--- src/libs/oauth/redux/reducer.js
+++ src/libs/oauth/redux/reducer.js
@@ -13,38 +13,42 @@ const initialStateSchema = {
   error: null 
 }
 
-const oauth2Reducer = ( state = initialStateSchema, action ) => {
-  switch ( action.type ) {
-    case OAUTH2_PENDING:
-      return { ...state, 
-        loggingIn: true
-      };
-    case OAUTH2_FULFILLED:
-      return { ...state, 
-        loggingIn: false, 
-        loggedIn: true, 
-        token: action.payload.token, 
-        type: action.payload.token_type, 
-        error: null 
-      };
-    case OAUTH2_REJECTED:
-      return { ...state, 
-        loggingIn: false,
-        loggedIn: false,
-        token: null, 
-        type: null, 
-        error: action.error
-      };
-    case OAUTH2_LOGOUT:
-      return { ...state, 
-        loggingIn: false,
-        loggedIn: false,
-        token: null, 
-        type: null, 
-        error: null 
-      };
-    default:
-      return state
+const oauth2Reducer = ( identifier = '' ) => {
+  return ( state = initialStateSchema, action ) => {
+    if ( action.identifier !== identifier ) return state;
+
+    switch ( action.type ) {
+      case OAUTH2_PENDING:
+        return { ...state, 
+          loggingIn: true
+        };
+      case OAUTH2_FULFILLED:
+        return { ...state, 
+          loggingIn: false, 
+          loggedIn: true, 
+          token: action.payload.token, 
+          type: action.payload.token_type, 
+          error: null 
+        };
+      case OAUTH2_REJECTED:
+        return { ...state, 
+          loggingIn: false,
+          loggedIn: false,
+          token: null, 
+          type: null, 
+          error: action.error
+        };
+      case OAUTH2_LOGOUT:
+        return { ...state, 
+          loggingIn: false,
+          loggedIn: false,
+          token: null, 
+          type: null, 
+          error: null 
+        };
+      default:
+        return state
+    }
   }
 }
 
